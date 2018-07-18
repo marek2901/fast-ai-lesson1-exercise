b5b3389bea6810bfbb6c8388916c34cf6e86e0c8
Marek Dziewit <marek2901@gmail.com>
2018-07-18 23:37:42 +0200

    Scrap google instead of duck duck go

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -1,84 +1,50 @@
+from bs4 import BeautifulSoup
 import requests
 import re
+import urllib.request
+import os
+import argparse
+import sys
 import json
-import pprint
-import time
 import pathlib
-import urllib
 
-def search(keywords):
-    url = 'https://duckduckgo.com/'
-    params = { 'q': keywords }
+def get_soup(url,header):
+    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
 
-    ## MAKE PATH for incoming images
-    pathlib.Path(f"./scrap_data/{keywords}/").mkdir(parents=True, exist_ok=True)
-    ##
-
-    #   First make a request to above URL, and parse out the 'vqd'
-    #   This is a special token, which should be used in the subsequent request
-    res = requests.post(url, data=params)
-    searchObj = re.search(r'vqd=(\d+)\&', res.text, re.M|re.I)
-
-    headers = {
-    'dnt': '1',
-    'accept-encoding': 'gzip, deflate, sdch, br',
-    'x-requested-with': 'XMLHttpRequest',
-    'accept-language': 'en-GB,en-USq=0.8,enq=0.6,msq=0.4',
-    'user-agent': 'Mozilla/5.0 (X11 Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    'accept': 'application/json, text/javascript, */* q=0.01',
-    'referer': 'https://duckduckgo.com/',
-    'authority': 'duckduckgo.com',
-    }
-
-    params = (
-    ('l', 'wt-wt'),
-    ('o', 'json'),
-    ('q', keywords),
-    ('vqd', searchObj.group(1)),
-    ('f', ',,,'),
-    ('p', '2')
-    )
-
-    requestUrl = url + "i.js"
-
-    while True:
-        res = requests.get(requestUrl, headers=headers, params=params)
-        data = json.loads(res.text)
-        # printJson(data["results"])
-        downloadImages(data["results"], keywords)
-        if "next" not in data:
-            break
-        requestUrl = url + data["next"]
-        time.sleep(5)
-
-def downloadImages(objs, dir_name):
-    for obj in objs:
+def main(args):
+    parser = argparse.ArgumentParser(description='Scrape Google images')
+    parser.add_argument('-s', '--search', default='bananas', type=str, help='search term')
+    parser.add_argument('-n', '--num_images', default=10, type=int, help='num images to save')
+    parser.add_argument('-d', '--folder', default='bananas', type=str, help='save directory')
+    args = parser.parse_args()
+    query = args.search#raw_input(args.search)
+    max_images = args.num_images
+    save_directory = f"./scrap_data/{args.folder}/"
+    pathlib.Path(save_directory).mkdir(parents=True, exist_ok=True)
+    query= query.split()
+    query='+'.join(query)
+    url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    soup = get_soup(url,header)
+    ActualImages=[]# contains the link for Large original images, type of  image
+    for a in soup.find_all("div",{"class":"rg_meta"}):
+        link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
+        ActualImages.append((link,Type))
+    for i , (img , Type) in enumerate( ActualImages[0:max_images]):
         try:
-            url = obj["image"]
-            filename = re.findall(r"[^\/]*",url)[-2]
-            urllib.request.urlretrieve(url, f"./scrap_data/{dir_name}/{filename}")
-            print(f"retreived {filename}")
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            print('Retriving image error')
-            next
-
-
-def printJson(objs):
-    for obj in objs:
-        print("Width {0}, Height {1}".format(obj["width"], obj["height"]))
-        print("Thumbnail {0}".format(obj["thumbnail"]))
-        print("Url {0}".format(obj["url"]))
-        print("Title {0}".format(obj["title"].encode('utf-8')))
-        print("Image {0}".format(obj["image"]))
-        print("__________")
-
-def main():
-    search('US Dollar')
-    # search('PLN nowe banknoty') ## ZAJEBISTY Keyword
-    # search('banknot złoty polski')
-
+            if len(Type)==0:
+                file_path = os.path.join(save_directory , "img" + "_"+ str(i)+".jpg")
+            else :
+                file_path = os.path.join(save_directory , "img" + "_"+ str(i)+"."+Type)
+            urllib.request.urlretrieve(img, file_path)
+        except Exception as e:
+            print(f"could not load : {img}")
+            print(e)
 
 if __name__ == '__main__':
-    main()
+    from sys import argv
+    try:
+        main(argv)
+    except KeyboardInterrupt:
+        pass
+    sys.exit()
