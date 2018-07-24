f29dc289576cf22d30d9755a91b5081da30f25c6
Marek Dziewit <marek2901@gmail.com>
2018-07-24 18:55:10 +0200

    data set tool final tweaks

--- create_data_set_tool/main.py
+++ create_data_set_tool/main.py
@@ -3,9 +3,10 @@ from pathlib import Path
 import shutil
 
 subsets_config = [
-    'test1',
-    'train',
-    'valid',
+    # name  , should group by kind
+    ('test1', False),
+    ('train', True),
+    ('valid', True),
 ]
 
 targed_dir = '../dataset/'
@@ -50,14 +51,24 @@ def main():
         while moved_images < ((num_of_images_per_subset * num_of_subsets) + rest_of_them):
             subset_index = min(num_of_subsets - 1, int(moved_images/num_of_images_per_subset))
 
-            dest_path = Path(f"{str(root_dataset_path)}/{subsets_config[subset_index]}/{kind}")
+            subset_name, should_group = subsets_config[subset_index]
+
+            if should_group:
+                dest_path = Path(f"{str(root_dataset_path)}/{subset_name}/{kind}")
+                dest_file = None
+            else:
+                new_file_name = f"{str(moved_images).zfill(6)}_{kind}.jpg"
+                dest_path = Path(f"{str(root_dataset_path)}/{subset_name}/")
+                dest_file = f"{str(root_dataset_path)}/{subset_name}/{new_file_name}"
+
+
             if not dest_path.exists():
                 dest_path.mkdir(parents=True, exist_ok=True)
 
             img_to_copy = images[moved_images][1]
 
             print(f"copying {img_to_copy} to {dest_path}")
-            shutil.copy(str(img_to_copy), str(dest_path))
+            shutil.copy(str(img_to_copy), str(dest_file or dest_path))
             moved_images += 1
 
 
